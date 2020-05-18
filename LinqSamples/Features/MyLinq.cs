f521e806e1a27b30b0f6d3d4801a8577abc2e7b4
daneelOlivaw76 <ruimgreis@gmail.com>
2020-05-18 17:15:02 +0100

    Middle of Module 3

---
+++ LinqSamples/Features/MyLinq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Features
+{
+    public static class MyLinq
+    {
+        /// <summary>
+        /// A new Count method for IEnumerables.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach(var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ LinqSamples/Features/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using static System.Console;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott"},
+                new Employee { Id = 2, Name = "Chris"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+
+            //foreach(var person in sales)
+            //{
+            //    WriteLine(person.Name);
+            //}
+
+            IEnumerator<Employee> enumerator1 = sales.GetEnumerator();
+            while (enumerator1.MoveNext())
+            {
+                WriteLine(enumerator1.Current.Name);
+            }
+            Console.WriteLine(sales.Count());
+
+            WriteLine("\n---\n");
+
+            IEnumerator<Employee> enumerator2 = developers.GetEnumerator();
+            while (enumerator2.MoveNext())
+            {
+                WriteLine(enumerator2.Current.Name);
+            }
+            Console.WriteLine(developers.Count());
+
+        }
+    }
+}
