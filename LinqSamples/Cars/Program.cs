418d6d5a13ad691065e16050ed867dfec9cfa5a4
daneelOlivaw76 <ruimgreis@gmail.com>
2020-05-21 18:13:05 +0100

    Ed of Module 6

---
+++ LinqSamples/Cars/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.WebSockets;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name);
+
+            var query2 = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Select(c => new
+                            {
+                                c.Manufacturer,
+                                c.Name,
+                                c.Combined
+                            });
+            var result = cars.SelectMany(c => c.Name)
+                             .OrderBy(c => c);
+
+            foreach(var c in result)
+            {
+                Console.WriteLine(c);
+            }
+
+            //var top = cars.OrderByDescending(c => c.Combined)
+            //                .ThenBy(c => c.Name)
+            //                .LastOrDefault(c => c.Manufacturer == "BMW" && c.Year == 2016);
+
+            //var result = cars.All(c => c.Manufacturer == "Ford");
+
+            //Console.WriteLine(result);
+
+            //Console.WriteLine($"{top.Manufacturer} {top.Name} : {top.Combined}");
+
+            //foreach (var car in query2.Take(10))
+            //{
+            //    Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            //}
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .ToCar()
+                    .ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3], CultureInfo.InvariantCulture),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+    }
+}
